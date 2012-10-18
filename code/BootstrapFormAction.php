bac389a180b6424fb38e2aae59cf7d1c7e277b72
Tim Klein <tim@dodat.de>
2012-10-17 23:44:01 -0700

    falling back to a more core compatible approach, using htmlattributes to support multiple form actions on a single form. simplified template and setting classes instead of custom variables

--- code/BootstrapFormAction.php
+++ code/BootstrapFormAction.php
@@ -9,6 +9,13 @@
  */
 class BootstrapFormAction extends DataExtension {
 
+	function setOwner($owner, $ownerBaseClass = null) {
+		parent::setOwner($owner, $ownerBaseClass);
+		if($owner) {
+			$owner->addExtraClass("btn");
+		}
+	}
+
 
 	/** 
 	 * Sets the style of button. Options include:
@@ -23,7 +30,7 @@ class BootstrapFormAction extends DataExtension {
 	 * @return BootstrapFormAction
 	 */
 	public function setStyle($style) {
-		$this->owner->ButtonStyle = trim(strtolower($style));
+		$this->owner->addExtraClass("btn-".trim(strtolower($style)));
 		return $this->owner;
 	}
 
@@ -39,7 +46,7 @@ class BootstrapFormAction extends DataExtension {
 	 * @return BootstrapFormAction
 	 */
 	public function setSize($size) {
-		$this->owner->ButtonSize = trim(strtolower($size));
+		$this->owner->addExtraClass("btn-".trim(strtolower($size)));
 		return $this->owner;
 	}
 
